a019155356d8e2813f8921d1aec856740f33aa72
Luis <luis.almajano@gmail.com>
2021-01-21 00:58:44 +0100

    Agregada connexion frontend-backend con libreria axios. Backend modificado para soportar cors

--- src/boats/pages/Boats.jsx
+++ src/boats/pages/Boats.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import axios from "axios";
 
 import SearchAppBar from "../../shared/components/Navigation/SearchAppBar";
 import SchiffListe from "../components/SchiffListe/SchiffListe";
 
 const Boats = () => {
-  const [boats_dummy, setBoats] = React.useState([{
+  const [boats_db, setBoats] = React.useState([]);
+
+  const [boats_dummy, setBoatsDummy] = React.useState([
+    {
       id: 1,
       name: "Tarragona",
       image: "https://picsum.photos/200",
@@ -25,18 +29,34 @@ const Boats = () => {
       timeseen: "2021-09-01",
       countseen: 1,
     },
-    
   ]);
 
   const addNewBoatHandler = (newBoat) => {
     const boats = boats_dummy.concat(newBoat);
-    setBoats(boats);
+    setBoatsDummy(boats);
   };
+
+  React.useEffect(() => {
+    /* 
+      The first .then() lets us get JSON data from the response.
+      The second .then() gets the url to my avatar and puts it in state. 
+    */
+    axios.get("http://localhost:3001/api/boats")
+    .then(result => setBoats(result.data))
+    .catch(error => console.error("Error fetching data with axios: ", error));
+
+    /* fetch("http://localhost:3001/api/boats")
+      .then((response) => response.json())
+      .then((data) => setBoats(data))
+      .catch((error) => console.error("Error fetching data: ", error));
+ */
+
+  }, []);
 
   return (
     <React.Fragment>
       <SearchAppBar onNewBoat={addNewBoatHandler} />
-      <SchiffListe boats={boats_dummy} />
+      <SchiffListe boats={boats_db} />
     </React.Fragment>
   );
 };
