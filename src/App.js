85639b802bfaefcc0216ee01c2061e78247ef445
Luis <luis.almajano@gmail.com>
2021-03-29 10:54:25 +0200

    Agregado codigo ProtectedRoute para proteger routes que necesiten auth. Funcionalidad todavia no activada en App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Boats from "./boats/pages/Boats";
 import Boat from "./boats/pages/Boat";
 import NewBoat from "./boats/pages/NewBoat";
 import Login from "./boats/pages/Login";
+import ProtectedRoute from './boats/pages/ProtectedRoute';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path="/" exact component={Boats} />
           <Route path="/login" exact component={Login} />
           <Route path="/boats/new" exact component={NewBoat} />
+          {/* <ProtectedRoute path='/boats/new' exact component={NewBoat} /> */}
           <Route path="/boats/:boatId" exact component={Boat} />
           <Redirect to="/" />
         </Switch>
